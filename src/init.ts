a3de47a15e17261a8f9721a636da3591b8ade26d
LyokoJeremie <lucheng989898@gmail.com>
2023-11-11 08:30:56 +0800

    .

--- src/init.ts
+++ src/init.ts
@@ -1,7 +1,7 @@
 import {SimpleCryptWrapper} from "./SimpleCryptWrapper";
 
-// window.modSimpleCryptWrapper = new SimpleCryptWrapper(window.modSC2DataManager, window.modUtils);
-// window.modSimpleCryptWrapper.init();
+window.modSimpleCryptWrapper = new SimpleCryptWrapper(window.modSC2DataManager, window.modUtils);
+window.modSimpleCryptWrapper.init();
 
-console.warn('SimpleCryptWrapper init done', window.modUtils.getNowRunningModName());
-window.modUtils.getLogger().warn(`SimpleCryptWrapper init done ${window.modUtils.getNowRunningModName()}`);
+// console.warn('SimpleCryptWrapper init done', window.modUtils.getNowRunningModName());
+// window.modUtils.getLogger().warn(`SimpleCryptWrapper init done ${window.modUtils.getNowRunningModName()}`);
