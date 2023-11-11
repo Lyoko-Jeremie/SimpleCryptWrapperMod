1f061d4402b4925892b3a0f596e94437cae154dc
LyokoJeremie <lucheng989898@gmail.com>
2023-11-11 08:48:18 +0800

    .

--- src/SimpleCryptWrapper.ts
+++ src/SimpleCryptWrapper.ts
@@ -13,28 +13,56 @@ export interface CryptDataItem {
     salt: string;
 }
 
-// TODO load from somewhere
-const ModName = 'CryptoI18n';
-
 export class SimpleCryptWrapper {
     private logger: LogWrapper;
+
+    private readonly ModName: string = '';
+
+    private readonly infoCreateOk = false;
 
     constructor(
         public gSC2DataManager: SC2DataManager,
         public gModUtils: ModUtils,
     ) {
         this.logger = gModUtils.getLogger();
+        if (gModUtils?.getSemVerTools && gModUtils.getSemVerTools()) {
+            const semVer = gModUtils.getSemVerTools();
+            if (semVer.satisfies(semVer.parseVersion(gModUtils.version).version, semVer.parseRange('>=2.5.1'))) {
+
+                // do init
+                const ModName = gModUtils.getNowRunningModName();
+                if (isString(ModName)) {
+
+                    this.ModName = ModName;
+                    this.infoCreateOk = true;
+
+                    return;
+                } else {
+                    this.logger.error(`[SimpleCryptWrapper] constructor failed, ModName is not string`);
+                    return;
+                }
+            } else {
+                this.logger.error(`[SimpleCryptWrapper] constructor failed, ModLoader version not match. need >=2.5.1`);
+            }
+        } else {
+            this.logger.error(`[SimpleCryptWrapper] constructor failed, ModLoader version not match. no getSemVerTools.`);
+        }
+        return;
     }
 
     async decrypt() {
+        if (!this.infoCreateOk) {
+            console.log(`[SimpleCryptWrapper] cannot call decrypt(), constructor not completed`);
+            this.logger.log(`[SimpleCryptWrapper] cannot call decrypt(), constructor not completed`);
+        }
         try {
-            console.log(`[${ModName}] decrypt`);
-            this.logger.log(`[${ModName}] decrypt`);
+            console.log(`[${this.ModName}] decrypt`);
+            this.logger.log(`[${this.ModName}] decrypt`);
             await ready;
             const mod = this.gSC2DataManager.getModLoader().getModByNameOne('CryptoI18n');
             if (!mod) {
-                console.error(`[${ModName}] Mod not found`);
-                this.logger.error(`[${ModName}] Mod not found`);
+                console.error(`[${this.ModName}] Mod not found`);
+                this.logger.error(`[${this.ModName}] Mod not found`);
                 return;
             }
 
@@ -52,8 +80,8 @@ export class SimpleCryptWrapper {
                     fileName = T.slice(0, -5);
                     typeName = 'salt';
                 } else {
-                    console.warn(`[${ModName}] Unknown file type`, T);
-                    this.logger.warn(`[${ModName}] Unknown file type [${T}]`);
+                    console.warn(`[${this.ModName}] Unknown file type`, T);
+                    this.logger.warn(`[${this.ModName}] Unknown file type [${T}]`);
                     return;
                 }
                 if (!cdi.has(fileName)) {
@@ -64,16 +92,16 @@ export class SimpleCryptWrapper {
             });
             for (const nn of cdi) {
                 if (!(nn[1].crypt && nn[1].nonce && nn[1].salt)) {
-                    console.warn(`[${ModName}] Missing file`, [nn]);
-                    this.logger.warn(`[${ModName}] Missing file [${nn[0]}]`);
+                    console.warn(`[${this.ModName}] Missing file`, [nn]);
+                    this.logger.warn(`[${this.ModName}] Missing file [${nn[0]}]`);
                     continue;
                 }
                 const crypt = await mod.zip.zip.file(nn[1].crypt)?.async('uint8array');
                 const nonce = await mod.zip.zip.file(nn[1].nonce)?.async('uint8array');
                 const salt = await mod.zip.zip.file(nn[1].salt)?.async('uint8array');
                 if (!(crypt && nonce && salt)) {
-                    console.warn(`[${ModName}] cannot get file from zip`, [nn, crypt, nonce, salt]);
-                    this.logger.warn(`[${ModName}] cannot get file from zip [${nn[0]}]`);
+                    console.warn(`[${this.ModName}] cannot get file from zip`, [nn, crypt, nonce, salt]);
+                    this.logger.warn(`[${this.ModName}] cannot get file from zip [${nn[0]}]`);
                     continue;
                 }
                 const key = await calcKeyFromPasswordBrowser(await this.readPassword(), salt);
@@ -82,36 +110,40 @@ export class SimpleCryptWrapper {
                     key,
                     nonce,
                 ).catch(async (E) => {
-                    console.error(`[${ModName}] decrypt error`, [nn, E]);
-                    this.logger.error(`[${ModName}] decrypt error [${nn[0]}] [${E?.message ? E.message : E}]`);
+                    console.error(`[${this.ModName}] decrypt error`, [nn, E]);
+                    this.logger.error(`[${this.ModName}] decrypt error [${nn[0]}] [${E?.message ? E.message : E}]`);
                     await window.modSweetAlert2Mod.fire(`解密失败，密码错误: [${E?.message ? E.message : E}]`);
                 });
                 if (!decryptZip) {
                     return;
                 }
                 if (!await this.gModUtils.lazyRegisterNewModZipData(decryptZip)) {
-                    console.error(`[${ModName}] cannot register new mod zip data`, [nn, decryptZip]);
-                    this.logger.error(`[${ModName}] cannot register new mod zip data [${nn[0]}]`);
+                    console.error(`[${this.ModName}] cannot register new mod zip data`, [nn, decryptZip]);
+                    this.logger.error(`[${this.ModName}] cannot register new mod zip data [${nn[0]}]`);
                 } else {
-                    console.log(`[${ModName}] decrypt success`, [nn]);
-                    this.logger.log(`[${ModName}] decrypt success [${nn[0]}]`);
+                    console.log(`[${this.ModName}] decrypt success`, [nn]);
+                    this.logger.log(`[${this.ModName}] decrypt success [${nn[0]}]`);
                 }
             }
         } catch (e: any) {
             console.error(e);
-            this.logger.error(`[${ModName}] decrypt () Error:[${e?.message ? e.message : e}]`);
+            this.logger.error(`[${this.ModName}] decrypt () Error:[${e?.message ? e.message : e}]`);
         }
     }
 
     // TODO get password from user input
     async readPassword() {
+        if (!this.infoCreateOk) {
+            console.log(`[SimpleCryptWrapper] cannot call readPassword(), constructor not completed`);
+            this.logger.log(`[SimpleCryptWrapper] cannot call readPassword(), constructor not completed`);
+        }
         // TODO
         try {
             const {value: password} = await window.modSweetAlert2Mod.fireWithOptions({
-                title: `请输入${ModName}的密码`,
+                title: `请输入${this.ModName}的密码`,
                 input: 'password',
                 inputLabel: '密码',
-                inputPlaceholder: `请输入${ModName}的密码`,
+                inputPlaceholder: `请输入${this.ModName}的密码`,
                 inputAttributes: {
                     maxlength: '1000',
                     autocapitalize: 'off',
@@ -131,13 +163,25 @@ export class SimpleCryptWrapper {
     }
 
     tryLoadPassword() {
+        if (!this.infoCreateOk) {
+            console.log(`[SimpleCryptWrapper] cannot call tryLoadPassword(), constructor not completed`);
+            this.logger.log(`[SimpleCryptWrapper] cannot call tryLoadPassword(), constructor not completed`);
+        }
         // try load from localstorage
     }
 
     savePassword() {
+        if (!this.infoCreateOk) {
+            console.log(`[SimpleCryptWrapper] cannot call savePassword(), constructor not completed`);
+            this.logger.log(`[SimpleCryptWrapper] cannot call savePassword(), constructor not completed`);
+        }
         // encrypt and save to localstorage
     }
 
     init() {
+        if (!this.infoCreateOk) {
+            console.log(`[SimpleCryptWrapper] cannot call init(), constructor not completed`);
+            this.logger.log(`[SimpleCryptWrapper] cannot call init(), constructor not completed`);
+        }
     }
 }
